34ffc56b93204376f3d2f9b93e226f5bc03b1934
s0uzqw <luis.ribeiro2@estudante.ifms.edu.br>
2024-10-22 08:13:43 -0400

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,48 @@
-import express from 'express';
-import cors from 'cors';
-import mysql from 'mysql2/promise';
+console.log(" Olá Mundo")
+//Live Server  é do Front-End
+//
+/*1 - Para construir um servidor back-end e responder 
+Vamos utilizar o EXPRESS */
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql2/promise'
+//Criar o Objeto do tipo express 
+const app = express ()
+//incluir para ele receber json
+app.use(express.json())//Middleware
+/*incluir o CORS -> Quando a Gente tem outra porta fazendo */
+app.use(cors())
+//Rota de produtos
 
-// Criar um objeto do tipo express.
-const app = express();
+app.get("/produtos", async(req,res)=>{
+    
+   // ok PASSO 1: Criar um banco de dados 
 
-// Middleware para receber JSON
-app.use(express.json());
+   // PASSO 2: Usar a lib mysql2 para conectar o banco
+   try{
+    const conexao = await mysql.createConnection({
+        /*OPERADOR TERNÁRIO*/
+        host: process.env.dbhost?process.env.dbhost: "localhost",
+        user: process.env.dbuser?process.env.dbuser: "root",
+        password: process.env.dbpassword?process.env.dbpassword: "",
+        database: process.env.dbname?process.env.dbname: "banco1022b",
+        port: process.env.dbport?parseInt(process.env.dbport): 3306
+    })
+     // PASSO 3: QUERY -> SELECT * FROM produtos 
+   const [result,filds] = await conexao.query("SELECT * from produtos")
+   
+   await conexao.end()
 
-// Incluir o CORS para permitir requisições de diferentes portas
-app.use(cors());
+    res.send(result)
+}catch(e){
+    res.status(500).send("Erro do servidor")
+}
 
-// Rota para obter produtos
-app.get("/produtos", async (req, res) => {
-    try {
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022b",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306,
-        });
+   // PASSO 4: Colocar os dados do banco no response
 
-        const [result, fields] = await conexao.query("SELECT * FROM produtos");
-        await conexao.end();
-        res.send(result);
-    } catch (e) {
-        res.status(500).send("Erro do servidor");
-    }
-});
+})
 
-// Rota para obter usuários
+// Nova rota para usuários
 app.get("/usuarios", async (req, res) => {
     try {
         const conexao = await mysql.createConnection({
@@ -51,7 +63,7 @@ app.get("/usuarios", async (req, res) => {
     }
 });
 
-// Iniciar o servidor
-app.listen(8000, () => {
-    console.log("SERVIDOR INICIADO NA PORTA 8000");
-});
+//INICIAR o Servidor 
+app.listen(8000,()=>{
+    console.log("SERVIDOR INICIADO NA PORTA 8000")
+})
