d8c26f776113e91edb2f5d544c05ec064e56e5ec
s0uzqw <luis.ribeiro2@estudante.ifms.edu.br>
2024-11-25 08:29:21 -0400

    Atualizações

--- src/main.ts
+++ src/main.ts
@@ -8,39 +8,46 @@ app.use(cors())
 
 
 app.get("/produtos", async (req, res) => {
+    let connection;
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022b",
+        connection = await mysql.createConnection({
+            host: process.env.dbhost || "localhost",
+            user: process.env.dbuser || "root",
+            password: process.env.dbpassword || "",
+            database: process.env.dbname || "banco1022b",
             port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        })
-        const [result, fields] = await connection.query("SELECT * from produtos")
-        await connection.end()
-        res.send(result)
+        });
+        const [result, fields] = await connection.query("SELECT * FROM produtos");
+        res.send(result);
     } catch (e) {
-        res.status(500).send("Server ERROR")
+        console.error(e); // Para logar o erro no console
+        res.status(500).send("Server ERROR");
+    } finally {
+        if (connection) await connection.end();
     }
-})
-
+});
 
 app.get("/usuarios", async (req, res) => {
+    let connection;
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022b",
+        connection = await mysql.createConnection({
+            host: process.env.dbhost || "localhost",
+            user: process.env.dbuser || "root",
+            password: process.env.dbpassword || "",
+            database: process.env.dbname || "banco1022b",
             port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        })
-        const [result, fields] = await connection.query("SELECT * from usuarios")
-        await connection.end()
-        res.send(result)
+        });
+
+        const [result] = await connection.query("SELECT * from usuarios");
+        res.send(result);
     } catch (e) {
-        res.status(500).send("Server ERROR")
+        console.error(e);  // Log do erro para entender o que está acontecendo
+        res.status(500).send({ message: "Server ERROR"}); // Retorna o erro detalhado para facilitar o debug
+    } finally {
+        if (connection) await connection.end();  // Certifica que a conexão será fechada
     }
-})
+});
+
 
 app.listen(8000, () => {
     console.log("Servidor Iniciado")
